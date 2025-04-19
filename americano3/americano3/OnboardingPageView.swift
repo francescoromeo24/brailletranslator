8dd450e9bcdee91c2713c406b38bffa4aa2e98f1
francescoromeo24 <francesco24602@outlook.it>
2025-04-19 13:08:16 +0200

    piccole modifiche + inizio supporto portogallo

---
+++ americano3/americano3/OnboardingPageView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPageView.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 19/04/25.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPageView()
+}
---
+++ americano3/americano3/models/OnboardingPage.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingPage.swift
+//  americano3
+//
+//  Created by Francesco Romeo on 19/04/25.
+//
+
+import Foundation
